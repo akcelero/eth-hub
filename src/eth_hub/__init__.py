afa11ec266f3d4d4468c173f34861c8d93e1e173
akcelero <akcelero@gmail.com>
2026-03-17 00:18:40 +0800

    🧹 chore: bump version to 1.1.0

--- src/eth_hub/__init__.py
+++ src/eth_hub/__init__.py
@@ -1,6 +1,6 @@
 """Flexible Ethereum key management and signing library."""
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __author__ = "akcelero"
 __email__ = "akcelero@gmail.com"
 __license__ = "MIT"
